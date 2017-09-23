3a32975edcc25f8c3b3e541e3df87175c4a9151b
Martin Balfanz <me@martinbalfanz.com>
2017-09-23 14:30:48 +0200

    Change scratch greating message.

--- settings/setup-emacs-lisp.el
+++ settings/setup-emacs-lisp.el
@@ -1,6 +1,9 @@
 (use-package elisp-slime-nav
-  :init
-  (dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
-    (add-hook hook 'turn-on-elisp-slime-nav-mode)))
+	:init
+	(dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
+		(add-hook hook 'turn-on-elisp-slime-nav-mode)))
+
+(setq-default initial-scratch-message
+							(concat ";; Happy hacking, " user-login-name " - Emacs ♥ you!\n\n"))
 
 (provide 'setup-emacs-lisp)
