b0f40f0fcb63aec0fe5afd1201ea1d8be8f145bc
Martin Balfanz <me@martinbalfanz.com>
2017-09-24 17:00:12 +0200

    Fix emacs lisp indentation.

--- settings/setup-emacs-lisp.el
+++ settings/setup-emacs-lisp.el
@@ -1,9 +1,9 @@
 (use-package elisp-slime-nav
-	:init
-	(dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
-		(add-hook hook 'turn-on-elisp-slime-nav-mode)))
+  :init
+  (dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
+    (add-hook hook 'turn-on-elisp-slime-nav-mode)))
 
 (setq-default initial-scratch-message
-							(concat ";; Happy hacking, " user-login-name " - Emacs ♥ you!\n\n"))
+	      (concat ";; Happy hacking, " user-login-name " - Emacs ♥ you!\n\n"))
 
 (provide 'setup-emacs-lisp)
