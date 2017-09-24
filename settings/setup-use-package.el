eeaa2fea6492e9d13816c8127f69112e6f00426b
Martin Balfanz <me@martinbalfanz.com>
2017-09-24 14:15:41 +0200

    Remove spacemacs dependency.

---
+++ settings/setup-use-package.el
@@ -0,0 +1,22 @@
+(require 'package)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(defconst sanityinc/no-ssl (and (memq system-type '(windows-nt ms-dos))
+                                (not (gnutls-available-p))))
+
+(add-to-list 'package-archives
+             `("melpa" . ,(if sanityinc/no-ssl
+                              "http://melpa.org/packages/"
+                            "https://melpa.org/packages/")))
+(unless sanityinc/no-ssl
+  ;; Force SSL for GNU ELPA
+  (setcdr (assoc "gnu" package-archives) "https://elpa.gnu.org/packages/"))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(setq use-package-always-ensure t)
+
+(provide 'setup-use-package)
