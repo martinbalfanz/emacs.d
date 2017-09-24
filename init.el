bdf3c2ae1833fcdae65fddeee8c6994f26d90c33
Martin Balfanz <me@martinbalfanz.com>
2017-09-24 16:51:04 +0200

    Move custom to separate file.

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 ;(package-initialize)
 
 (add-to-list 'load-path (expand-file-name "settings" user-emacs-directory))
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
 (require 'setup-use-package)
 
@@ -26,17 +27,4 @@
 (require 'setup-vue)
 
 (provide 'my-setup)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (web-mode parinfer elisp-slime-nav magit-gh-pulls github-issues github-clone bug-reference-github yagist diff-hl magit fullframe git-timemachine gitconfig-mode git-blamed switch-window whitespace-cleanup-mode helm-company helm-gitignore helm-ls-git helm-swoop helm-tramp helm-descbinds helm-ag helm-projectile helm-mu helm-clojuredocs helm-dash helm move-dup page-break-lines multiple-cursors avy expand-region symbol-overlay rainbow-delimiters browse-kill-ring undo-tree list-unicode-display unfill color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized exec-path-from-shell use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
