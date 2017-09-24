303731656776541b05df9fa32c4f8b1badff7cf0
Martin Balfanz <me@martinbalfanz.com>
2017-09-24 20:47:57 +0200

    Extract company out of elisp-slime-nav.

--- settings/setup-emacs-lisp.el
+++ settings/setup-emacs-lisp.el
@@ -2,8 +2,10 @@
   :diminish elisp-slime-nav-mode
   :init
   (dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
-    (add-hook hook 'turn-on-elisp-slime-nav-mode)
-    (add-hook hook 'company-mode-on)))
+    (add-hook hook 'turn-on-elisp-slime-nav-mode)))
+
+(dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
+  (add-hook hook 'company-mode-on))
 
 (setq-default initial-scratch-message
 	      (concat ";; Happy hacking, " user-login-name " - Emacs ♥ you!\n\n"))
