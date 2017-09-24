733d321707ad25f1c81d24b391ae834a5ddf8133
Martin Balfanz <me@martinbalfanz.com>
2017-09-24 14:30:04 +0200

    Fix indentation of lisp setup and add paredit.

--- settings/setup-lisp.el
+++ settings/setup-lisp.el
@@ -1,24 +1,26 @@
 (defun mb/remove-smartparens-and-add-parinfer ()
-	"If smartparens mode is enabled, disable it and enable parinfer instead."
-	(when (featurep 'smartparens)
-		(smartparens-mode -1))
-	(parinfer-mode t))
+  "If smartparens mode is enabled, disable it and enable parinfer instead."
+  (when (featurep 'smartparens)
+    (smartparens-mode -1))
+  (parinfer-mode t))
+
+(use-package paredit)
 
 (use-package parinfer
-	:bind ("C-," . parinfer-toggle-mode)
+  :bind ("C-," . parinfer-toggle-mode)
   :diminish parinfer-mode
-	:init
-	(setq parinfer-extensions
-				'(defaults
-					 pretty-parens
-					 paredit
-					 smart-tab
-					 smart-yank))
-	(add-hook 'clojure-mode-hook 'mb/remove-smartparens-and-add-parinfer)
-	(add-hook 'emacs-lisp-mode-hook 'mb/remove-smartparens-and-add-parinfer)
-	(add-hook 'common-lisp-mode-hook 'mb/remove-smartparens-and-add-parinfer)
-	(add-hook 'scheme-mode-hook 'mb/remove-smartparens-and-add-parinfer)
-	(add-hook 'lisp-mode-hook 'mb/remove-smartparens-and-add-parinfer)
+  :init
+  (setq parinfer-extensions
+	'(defaults
+	   pretty-parens
+	   paredit
+	   smart-tab
+	   smart-yank))
+  (add-hook 'clojure-mode-hook 'mb/remove-smartparens-and-add-parinfer)
+  (add-hook 'emacs-lisp-mode-hook 'mb/remove-smartparens-and-add-parinfer)
+  (add-hook 'common-lisp-mode-hook 'mb/remove-smartparens-and-add-parinfer)
+  (add-hook 'scheme-mode-hook 'mb/remove-smartparens-and-add-parinfer)
+  (add-hook 'lisp-mode-hook 'mb/remove-smartparens-and-add-parinfer)
   (add-hook 'json-mode-hook 'mb/remove-smartparens-and-add-parinfer))
 
 (provide 'setup-lisp)
