570d32f945251a7de4cb6426a6e0dff2572256ef
Martin Balfanz <me@martinbalfanz.com>
2017-09-25 15:14:43 +0200

    Fix indentation for setup-vue.el

--- settings/setup-vue.el
+++ settings/setup-vue.el
@@ -4,10 +4,10 @@
   web-mode "Vue"
   "Major mode for vue-js files"
   (setq web-mode-markup-indent-offset 2
-        web-mode-css-indent-offset 2
-        web-mode-code-indent-offset 2
-        web-mode-style-padding 0
-        web-mode-script-padding 0))
+	web-mode-css-indent-offset 2
+	web-mode-code-indent-offset 2
+	web-mode-style-padding 0
+	web-mode-script-padding 0))
 
 (add-to-list 'auto-mode-alist '("\\.vue$" . vue-mode))
 
