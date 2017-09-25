e09aa11a1b28b9dd58d90588d0b6dc8d57365018
Martin Balfanz <me@martinbalfanz.com>
2017-09-25 08:07:48 +0200

    Add purcell's startup performance tweak

--- init.el
+++ init.el
@@ -1,5 +1,14 @@
 ;(package-initialize)
 
+;; Temporarily reduce garbage collection during startup
+(defconst sanityinc/initial-gc-cons-threshold gc-cons-threshold
+  "Initial value of `gc-cons-threshold' at start-up time.")
+(setq gc-cons-threshold (* 128 1024 1024))
+(add-hook 'after-init-hook
+	  (lambda () (setq gc-cons-threshold sanityinc/initial-gc-cons-threshold)))
+
+;; ------------------------------------------------------------
+
 (add-to-list 'load-path (expand-file-name "settings" user-emacs-directory))
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
