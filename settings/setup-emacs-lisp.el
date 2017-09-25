fb2aa87e734154f4d59c47d2fc6f218ccb3272ff
Martin Balfanz <me@martinbalfanz.com>
2017-09-25 21:29:15 +0200

    Refactor emacs lisp setup

--- settings/setup-emacs-lisp.el
+++ settings/setup-emacs-lisp.el
@@ -1,14 +1,20 @@
 (use-package elisp-slime-nav
   :diminish elisp-slime-nav-mode
   :init
-  (dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
+  (dolist (hook '(emacs-lisp-mode-hook
+                  ielm-mode-hook))
     (add-hook hook 'turn-on-elisp-slime-nav-mode)))
 
-(dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
-  (when (featurep 'company)
-    (add-hook hook 'company-mode-on)))
+(dolist (hook '(emacs-lisp-mode-hook
+                ielm-mode-hook))
+  (add-hook 'hook (lambda ()
+                   (when (featurep 'company)
+                     (add-hook hook 'company-mode-on))
+                   (eldoc-mode 1))))
+
+(diminish 'eldoc-mode)
 
 (setq-default initial-scratch-message
-	      (concat ";; Happy hacking, " user-login-name " - Emacs ♥ you!\n\n"))
+              (concat ";; Happy hacking, " user-login-name " - Emacs ♥ you!\n\n"))
 
 (provide 'setup-emacs-lisp)
