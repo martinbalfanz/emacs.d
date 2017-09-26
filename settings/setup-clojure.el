695d1207b65ac2a380b9f2c34437a661ac203cf7
Martin Balfanz <me@martinbalfanz.com>
2017-09-26 21:47:05 +0200

    Little extension to clojure config.

--- settings/setup-clojure.el
+++ settings/setup-clojure.el
@@ -1,4 +1,6 @@
 (use-package clojure-mode
+  :mode (("\\.clj$" . clojure-mode)
+         ("\\.cljs$" . clojure-mode))
   :config
   (add-hook 'clojure-mode-hook 'subword-mode))
 
@@ -7,6 +9,13 @@
 (use-package cider
   :config
   (add-hook 'cider-mode-hook 'eldoc-mode)
+  (add-hook 'cider-repl-mode-hook 'eldoc-mode)
+  (add-hook 'cider-clojure-interaction-mode-hook 'eldoc-mode)
+  (add-hook 'cider-mode-hook 'subword-mode)
   (add-hook 'cider-repl-mode-hook 'subword-mode))
+
+(use-package clj-refactor
+  :init
+  (add-hook 'clojure-mode-hook 'clj-refactor-mode))
 
 (provide 'setup-clojure)
