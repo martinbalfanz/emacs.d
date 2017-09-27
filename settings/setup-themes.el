e47fc4ad914d7d7b60f2a895118498b593678337
Martin Balfanz <me@martinbalfanz.com>
2017-09-27 21:06:07 +0200

    Bringing back old spaceline setup, disabled.

--- settings/setup-themes.el
+++ settings/setup-themes.el
@@ -9,6 +9,13 @@
   (setq powerline-default-separator 'wave)
   (spaceline-spacemacs-theme))
 
+(use-package spaceline-all-the-icons
+  :disabled
+  :after spaceline
+  :config
+  (setq spaceline-all-the-icons-separator-type 'arrow)
+  (spaceline-all-the-icons-theme))
+
 (setq-default line-spacing 2)
 
 (provide 'setup-themes)
