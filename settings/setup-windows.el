4e78fa77458cb34e42c0b13e9f74f6b5c986d0bc
Martin Balfanz <me@martinbalfanz.com>
2017-10-05 10:04:50 +0200

    Change switch-window shortcut to qwerty

--- settings/setup-windows.el
+++ settings/setup-windows.el
@@ -3,7 +3,7 @@
 (use-package switch-window
   :bind ("C-x o" . switch-window)
   :config
-  (setq-default switch-window-shortcut-style 'alphabet)
+  (setq-default switch-window-shortcut-style 'qwerty)
   (setq-default switch-window-timeout nil))
 
 
