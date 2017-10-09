a1f66e378ca8829471a1d93f759e91720dde30f1
Martin Balfanz <me@martinbalfanz.com>
2017-10-09 13:31:54 +0200

    Trying global company mode.

--- settings/setup-completion.el
+++ settings/setup-completion.el
@@ -2,11 +2,13 @@
 
 (use-package company
   :diminish company-mode
+  :bind ("C-c /" . company-complete)
   :config
   (setq company-idle-delay 0.2
         company-dabbrev-downcase nil
         company-dabbrev-other-buffers t
-        company-tooltip-align-annotations t))
+        company-tooltip-align-annotations t)
+  (global-company-mode 1))
 
 (use-package pos-tip)
 (use-package company-quickhelp
