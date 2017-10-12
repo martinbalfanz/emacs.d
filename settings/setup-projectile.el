8b00abead411ddcad6afbb5c98b8218c2bc5f06f
Martin Balfanz <me@martinbalfanz.com>
2017-10-12 12:27:19 +0200

    Add projectile-ripgrep

--- settings/setup-projectile.el
+++ settings/setup-projectile.el
@@ -4,5 +4,10 @@
   (add-hook 'after-init-hook 'projectile-mode))
 
 (use-package ag)
+(use-package projectile-ripgrep
+  :if (executable-find "rg")
+  :after projectile
+  :bind (:map projectile-mode-map
+              ("C-c p s r" . projectile-ripgrep))))
 
 (provide 'setup-projectile)
