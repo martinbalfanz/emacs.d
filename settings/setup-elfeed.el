bf6b0d90013396bdf07f8bd4b1bc7d34c8502e91
Martin Balfanz <me@martinbalfanz.com>
2017-11-22 10:23:30 +0100

    Change show-entry behaviour for elfeed-goodies split view.

--- settings/setup-elfeed.el
+++ settings/setup-elfeed.el
@@ -3,8 +3,19 @@
 
 (use-package elfeed-goodies
   :after elfeed
+  :bind (:map elfeed-search-mode-map
+              ("RET" . mb/elfeed-search-show-entry))
   :config
-  (elfeed-goodies/setup))
+  (elfeed-goodies/setup)
+
+  (defun mb/elfeed-search-show-entry (entry)
+    "Display the currently selected item in a buffer, but don't skip a line forward."
+    (interactive (list (elfeed-search-selected :ignore-region)))
+    (require 'elfeed-show)
+    (when (elfeed-entry-p entry)
+      (elfeed-untag entry 'unread)
+      (elfeed-search-update-entry entry)
+      (elfeed-show-entry entry))))
 
 (use-package elfeed-org
   :after elfeed
