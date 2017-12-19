bedd7b6fadf6076955dc8044421f4e829aeacb58
Martin Balfanz <me@martinbalfanz.com>
2017-12-19 13:14:11 +0100

    Setup autopep8.

--- settings/setup-python.el
+++ settings/setup-python.el
@@ -11,4 +11,14 @@
               (lambda ()
                 (flycheck-mode 1)
                 (setq-local flycheck-check-syntax-automatically '(mode-enabled save))))))
+
+(use-package py-autopep8
+  :ensure-system-package (autopep8 . "pip install autopep8")
+  :after elpy
+  :init
+  (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save))
+
+;; (use-package ein)
+;; (use-package company-jedi)
+
 (provide 'setup-python)
