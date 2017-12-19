9c2ce334f9bcc2a4e6d407353c155547a0b30476
Martin Balfanz <me@martinbalfanz.com>
2017-12-19 13:13:58 +0100

    Setup elpy

--- settings/setup-python.el
+++ settings/setup-python.el
@@ -1,3 +1,14 @@
 (add-hook 'python-mode-hook 'subword-mode)
 
+(use-package elpy
+  :defer 10
+  :config
+  (elpy-enable)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+
+  (when (featurep 'flycheck)
+    (add-hook 'elpy-mode-hook
+              (lambda ()
+                (flycheck-mode 1)
+                (setq-local flycheck-check-syntax-automatically '(mode-enabled save))))))
 (provide 'setup-python)
