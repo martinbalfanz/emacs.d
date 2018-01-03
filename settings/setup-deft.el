2426d02d210af09300ffbc0f0d2f76b195bd2199
Martin Balfanz <me@martinbalfanz.com>
2018-01-03 22:25:25 +0100

    Refactor deft setup

--- settings/setup-deft.el
+++ settings/setup-deft.el
@@ -2,22 +2,17 @@
   :commands (deft)
   :bind (:map deft-mode-map
               ("C-n" . deft-next-line)
-              ("C-p" . deft-previous-line)
-              ("C-u C-<return>" . deft-new-file-default-name))
+              ("C-p" . deft-previous-line))
   :config
   (add-hook 'deft-mode-hook 'hl-line-mode)
 
-  (defun deft-default-filename ()
-    "Default filename for new files without SLUG."
-    (format-time-string "%Y-%m-%d-%H%M%S"))
-
   (setq deft-extensions '("org" "markdown" "md" "tex" "txt")
+        deft-default-extension "org"
         deft-recursive t
         deft-text-mode 'org-mode
-        deft-strip-title-regexp "^#.TITLE:[ ]*"
         deft-auto-save-interval 15.0
         deft-use-filename-as-title t
-        deft-directory "~/Dropbox/notes")
+        deft-new-file-format "%Y-%m-%d-%H%M%S")
 
   (defun deft-next-line ()
     "Move cursor to next line and open file in other window."
@@ -31,24 +26,16 @@
     (forward-line -1)
     (deft-open-file-other-window))
 
-  (defun deft-new-file-default-name ()
-    "Create file with default file name."
-    (interactive)
-    (deft-new-file-named (deft-default-filename)))
-
-  (defun deft-parse-summary (contents title)
-    "Parse the file CONTENTS and extract a summary.
-The summary is a string extracted from the contents following the
-title without comments."
-    (let* ((summary-without-comments (replace-regexp-in-string "^#.*$" "" contents))
-           (summary (replace-regexp-in-string "[\n\t ]+" " " summary-without-comments)))
-      summary))
-
   (defadvice deft-new-file-named
       (after mb-deft-insert-org-template last () activate)
     "Create new file and insert org-mode template."
     (let* ((timestamp (concat "[" (format-time-string "%Y-%m-%d %H:%M") "]"))
-           (tmpl (concat "#+TITLE:
+           (filter (deft-whole-filter-regexp))
+           (title (lambda ()
+                    (if (string-empty-p filter)
+                        (ad-get-arg 0)
+                      filter)))
+           (tmpl (concat "#+TITLE: " (eval title) "
 #+DATE: " timestamp "
 #+KEYWORDS:
 
