df8b6fb9b748d8b074aa63ee9b3d4a6a0a5d726f
Martin Balfanz <me@martinbalfanz.com>
2018-01-05 11:22:31 +0100

    Remove unused capture templates

--- settings/setup-org.el
+++ settings/setup-org.el
@@ -41,16 +41,10 @@
            "* %?\n:PROPERTIES:\n:CREATED: %U\n:ORIGIN: [[%F]]\n:FULL_ORIGIN: %a\n:END:\n\n%i\n" :clock-in t :clock-resume t)
           ("j" "Journal" entry (file+datetree "journal.org")
            "* %U\n\n%?\n" :clock-in t :clock-resume t :kill-buffer t)
-          ("r" "respond" entry (file "~/git/org/refile.org")
-           "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
           ("m" "Meeting" entry (file+headline "inbox.org" "Meeting Minutes")
            "* MEETING %?\n:PROPERTIES:\n:CREATED: %T\n:ORIGIN: [[%F]]\n:FULL_ORIGIN: %a\n:END:\n\nParticipants\n- \n%i\n" :clock-in t :clock-resume t)
           ("p" "Call" entry (file+headline "inbox.org" "Phone Calls")
-           "* PHONE %? :phone:\n:PROPERTIES:\n:CREATED: %T\n:ORIGIN: [[%F]]\n:FULL_ORIGIN: %a\n:END:\n\n%i\n" :clock-in t :clock-resume t)
-          ;; ("h" "Habit" entry (file "~/git/org/refile.org")
-          ;;  "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n")
-          ("c" "Capture website" entry (file+headline "bookmarkx.org" "Unsorted")
-           "* %?%:description\n:PROPERTIES:\n:URL: %:link\n:END:\n\n Added %U\n\n%:initial" :immediate-finish t)))
+           "* PHONE %? :phone:\n:PROPERTIES:\n:CREATED: %T\n:ORIGIN: [[%F]]\n:FULL_ORIGIN: %a\n:END:\n\n%i\n" :clock-in t :clock-resume t)))
 
 
   (defun mb/require-final-newline ()
