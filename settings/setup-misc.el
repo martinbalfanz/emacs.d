b6691aeeafa7f7b4882ae7c09e889ac7cfdef371
Martin Balfanz <mbalfanz@mozilla.com>
2018-03-09 12:57:47 +0100

    Auto-save buffer on buffer switch, borrowed from prelude

--- settings/setup-misc.el
+++ settings/setup-misc.el
@@ -99,4 +99,33 @@ Deletes whitespace at join."
          (package-menu-mode . stripe-buffer-mode)
          (deft-mode . stripe-buffer-mode)))
 
+;; ------------------------------------------------------------
+;; from prelude:
+;; automatically save buffers associated with files on buffer switch
+;; and on windows switch
+(defun prelude-auto-save-command ()
+  "Save the current buffer if `prelude-auto-save' is not nil."
+  (when (and ;; prelude-auto-save
+         buffer-file-name
+         (buffer-modified-p (current-buffer))
+         (file-writable-p buffer-file-name))
+    (save-buffer)))
+
+(defmacro advise-commands (advice-name commands class &rest body)
+  "Apply advice named ADVICE-NAME to multiple COMMANDS.
+The body of the advice is in BODY."
+  `(progn
+     ,@(mapcar (lambda (command)
+                 `(defadvice ,command (,class ,(intern (concat (symbol-name command) "-" advice-name)) activate)
+                    ,@body))
+               commands)))
+
+;; advise all window switching functions
+(advise-commands "auto-save"
+                 (switch-to-buffer other-window windmove-up windmove-down windmove-left windmove-right)
+                 before
+                 (prelude-auto-save-command))
+
+(add-hook 'mouse-leave-buffer-hook 'prelude-auto-save-command)
+
 (provide 'setup-misc)
